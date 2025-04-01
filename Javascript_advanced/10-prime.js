3718a25fe63a67cd62cf35268bdbe6b0d148c466
Paintballskaguy <Johnstournament@gmail.com>
2025-04-01 12:45:47 -0500

    my pc did the 100x in 2 milseconds...

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+// for loops for prime tester //
+function countPrimeNumbers() {
+    let count = 0;
+    for (let num = 2; num <= 100; num++) {
+        let isPrime = true;
+
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// measure execution time // 
+const startTime = performance.now();
+
+// 100x loop counter //
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+const endTime = performance.now();
+
+// calculate and log execution time //
+const executionTime = endTime - startTime;
+console.log(`Execution time of printing countPrimeNumbers 100 times was ${executionTime} milliseconds.`);
