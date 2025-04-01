9539c963fc5b207ba18675e3c4b91bb941b11adf
Paintballskaguy <Johnstournament@gmail.com>
2025-04-01 12:28:43 -0500

    payment procesor - misspelling and correct variable names are important

--- Javascript_advanced/7-timeout.js
+++ Javascript_advanced/7-timeout.js
@@ -1,9 +1,10 @@
 console.log(`Start of the execution queue`);
 
+// timer set for the console.log to print // 
 setTimeout(() => {
     console.log(`Final code block to be executed`);
 }, 0);
-
+// for loop that counts up to 100 //
 for (let i = 1; i <= 100; i++) {
   console.log(i);
 }
